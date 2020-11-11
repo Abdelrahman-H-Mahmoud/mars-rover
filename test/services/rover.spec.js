21ce2a95878087620b7937353ce1deb59b7e6989
Abdelrahman Hussein <abdelrahman.h.mahmoued@gmail.com>
2020-11-11 22:30:56 +0200

    feat: init rover service unit testing

---
+++ test/services/rover.spec.js
@@ -0,0 +1,14 @@
+const Rover = require('../../src/services/rover');
+const DIRECTIONS = require('../../src/const/directions');
+const validateString = require('../../src/util/validateString');
+describe("Validate Rover Service", () => {
+  let rover = null;
+  const initCoordinates = {
+    x: 4,
+    y: 2,
+    direction: DIRECTIONS.EAST
+  };
+  beforeEach(function (done) {
+    rover = new Rover(initCoordinates, validateString);
+  });
+});
