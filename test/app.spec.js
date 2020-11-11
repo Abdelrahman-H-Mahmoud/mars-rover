61a030ec2a272ee29d911b7eee1567f5cd0574aa
Abdelrahman Hussein <abdelrahman.h.mahmoued@gmail.com>
2020-11-12 00:10:53 +0200

    feat: rover move unit test status 200

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,16 +1,35 @@
+const { expect } = require('chai');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require("../src/app");
+const DIRECTIONS = require('../src/const/directions');
+
 chai.use(chaiHttp);
 chai.should();
 
-describe("ROVER API", () => {
+describe("ROVER API", (done) => {
   it("Should return 400 if pattern invalid", (done) => {
     const movePattern = "FBLCRX";
     chai.request(server)
       .get(`/api/rover/${movePattern}`)
       .end((err, res) => {
         res.should.have.status(400);
+        done();
+      });
+  });
+
+  it("Should return 200 and current position if pattern is valid  ", (done) => {
+    const movePattern = "FLFFFRFLB";
+    const expected = {
+      x: 6,
+      y: 4,
+      direction: DIRECTIONS.NORTH.value
+    }
+    chai.request(server)
+      .get(`/api/rover/${movePattern}`)
+      .end((err, res) => {
+        res.should.have.status(200);
+        expect(res.body).to.deep.equal(expected);
         done();
       });
   });
