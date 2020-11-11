8d1f9fcfd23a7fe2fd41e5b41992af03b740826c
Abdelrahman Hussein <abdelrahman.h.mahmoued@gmail.com>
2020-11-11 23:01:04 +0200

    feat: add more tests to rove service

--- test/services/rover.spec.js
+++ test/services/rover.spec.js
@@ -1,6 +1,7 @@
 const Rover = require('../../src/services/rover');
 const DIRECTIONS = require('../../src/const/directions');
 const validateString = require('../../src/util/validateString');
+const { expect } = require('chai');
 describe("Validate Rover Service", () => {
   let rover = null;
   const initCoordinates = {
@@ -8,7 +9,19 @@ describe("Validate Rover Service", () => {
     y: 2,
     direction: DIRECTIONS.EAST.value
   };
+
   beforeEach(function (done) {
     rover = new Rover(initCoordinates, validateString);
+    done();
+  });
+
+  it("move=> Should return null if direction pattern wrong", () => {
+    expect(rover.move("FBLCRX")).to.be.null;
+  });
+
+  it("move=> Should return direction Object if direction pattern correct", () => {
+    expect(rover.move("FLFFFRFLB")).to.have.own.property('x');
+    expect(rover.move("FLFFFRFLB")).to.have.own.property('y');
+    expect(rover.move("FLFFFRFLB")).to.have.own.property('direction');
   });
 });
