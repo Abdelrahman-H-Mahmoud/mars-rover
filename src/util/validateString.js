b27aa3026f697857ea936b0202167fa86d272685
Abdelrahman Hussein <abdelrahman.h.mahmoued@gmail.com>
2020-11-11 21:56:14 +0200

    feat: add unit testing & logic

--- src/util/validateString.js
+++ src/util/validateString.js
@@ -1,6 +1,13 @@
+/**
+ * 
+ * @param {string} str
+ * @returns {boolean} if the string is valid
+ */
 const validateString = (str) => {
   if (!str || typeof str !== "string")
     return false;
+  let strRegex = new RegExp("^[FBLR]+$", "i");
+  return strRegex.test(str);
 }
 
 module.exports = validateString;
--- test/util/validateString.spec.js
+++ test/util/validateString.spec.js
@@ -4,5 +4,13 @@ const validateString = require('../../src/util/validateString');
 describe("validate String util function", () => {
   it ("Should return false if string is empty",()=>{
     expect(validateString("")).to.be.false;
-  })
+  });
+  it("Should return false if the string not one of this character[F,B,L,R]", () => {
+    let str = "FBLCRX";
+    expect(validateString(str)).to.be.false;
+  });
+  it("Should return true if the string is one of this character[F,B,L,R]", () => {
+    let str = "FLFFFRFLB";
+    expect(validateString(str)).to.be.true;
+  });
 });
