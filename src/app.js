09aa594b065449addb27d856ab46c41cd6179fba
Abdelrahman Hussein <abdelrahman.h.mahmoued@gmail.com>
2020-11-12 00:11:16 +0200

    feat: handle rover API business

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
 const stringValidator = require('./util/validateString');
+const Rover = require('./services/rover');
+const DIRECTIONS = require('./const/directions');
+
+const initCoordinates = {
+  x: 4,
+  y: 2,
+  direction: DIRECTIONS.EAST.value
+};
+const garvis = new Rover({ ...initCoordinates }, stringValidator)
 
 const validateMiddleware = (req, res, next) => {
   if (stringValidator(req.params.movePattern)) {
-    next();
+    return next();
   }
   return res.status(400).json({ message: "Invalid move pattern" });
 }
 app.get("/api/rover/:movePattern", validateMiddleware, validateMiddleware, (req, res) => {
+  const pattern = req.params.movePattern;
+  return res.json(garvis.move(pattern));
 });
 
 app.listen(3000, () => {
