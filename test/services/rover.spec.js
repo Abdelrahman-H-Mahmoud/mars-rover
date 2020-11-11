289a81ceed453cb04458be7081ea598400efaf4e
Abdelrahman Hussein <abdelrahman.h.mahmoued@gmail.com>
2020-11-11 23:31:15 +0200

    fix: copy object

--- test/services/rover.spec.js
+++ test/services/rover.spec.js
@@ -11,17 +11,29 @@ describe("Validate Rover Service", () => {
   };
 
   beforeEach(function (done) {
-    rover = new Rover(initCoordinates, validateString);
+    rover = new Rover(Object.assign({}, initCoordinates), validateString);
     done();
   });
 
-  it("move=> Should return null if direction pattern wrong", () => {
+  it("move=> Should return null if direction pattern wrong", function (done) {
     expect(rover.move("FBLCRX")).to.be.null;
+    done();
   });
 
-  it("move=> Should return direction Object if direction pattern correct", () => {
+  it("move=> Should return direction Object if direction pattern correct", function (done) {
     expect(rover.move("FLFFFRFLB")).to.have.own.property('x');
     expect(rover.move("FLFFFRFLB")).to.have.own.property('y');
     expect(rover.move("FLFFFRFLB")).to.have.own.property('direction');
+    done();
+  });
+
+  it("move=> Should return direction Object with the current position", function (done) {
+    const expected = {
+      x: 6,
+      y: 4,
+      direction: DIRECTIONS.NORTH.value
+    }
+    expect(rover.move("FLFFFRFLB")).to.deep.equal(expected);
+    done();
   });
 });
