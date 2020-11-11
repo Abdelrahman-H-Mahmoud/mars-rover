c933c058ad4ad947ede5bdb9570dc86eb52812c4
Abdelrahman Hussein <abdelrahman.h.mahmoued@gmail.com>
2020-11-11 23:33:43 +0200

    feat: add move business functionality

--- src/services/rover.js
+++ src/services/rover.js
@@ -1,3 +1,4 @@
+const DIRECTIONS = require('../const/directions');
 class Rover {
 
   /**
@@ -9,9 +10,39 @@ class Rover {
     this.moveStrValidator = moveStrValidator;
   }
 
+  /**
+   * 
+   * @param {string} movePattern 
+   */
   move(movePattern) {
     if (!this.moveStrValidator(movePattern)) {
       return null;
+    }
+    for (let i = 0; i < movePattern.length; i++) {
+      const char = movePattern[i];
+      let currDir = null;
+      switch (char) {
+        case "F":
+        case "f":
+          currDir = DIRECTIONS[this.initCoordinates.direction];
+          this.initCoordinates[currDir.dir]++;
+          break;
+        case "B":
+        case "b":
+          currDir = DIRECTIONS[this.initCoordinates.direction];
+          this.initCoordinates[currDir.dir]--;
+          break;
+        case "L":
+        case "l":
+          currDir = DIRECTIONS[this.initCoordinates.direction];
+          this.initCoordinates.direction = currDir.prev;
+          break;
+        case "R":
+        case "r":
+          currDir = DIRECTIONS[this.initCoordinates.direction];
+          this.initCoordinates.direction = currDir.next;
+          break;
+      }
     }
     return this.initCoordinates;
   }
