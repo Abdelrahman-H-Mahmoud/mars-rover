6878fec22d743e8e1a357a3408a6b62272dedd18
Abdelrahman Hussein <abdelrahman.h.mahmoued@gmail.com>
2020-11-21 21:57:51 +0200

    refactor: moveRight unit test to cover 360 degree

--- test/util/movingStrategies.spec.js
+++ test/util/movingStrategies.spec.js
@@ -3,15 +3,36 @@ const { moveRight } = require("../../src/util/movingStrategies");
 const DIRECTIONS = require('../../src/const/directions');
 
 describe("moving strategies",()=>{
-  it("should move right and the new direction is south",(done)=>{
+  describe("move right strategy",()=>{
     let currCoordinates={
       x: 4,
       y: 2,
       direction: DIRECTIONS.EAST.value
     };
-    let newCoordinates=moveRight(currCoordinates);
-    expect(newCoordinates).to.have.own.property('direction');
-    expect(newCoordinates.direction).to.equal(DIRECTIONS.SOUTH.value);
-    done();
+    it("should move right and the new direction is south",(done)=>{
+      moveRight(currCoordinates);
+      expect(currCoordinates).to.have.own.property('direction');
+      expect(currCoordinates.direction).to.equal(DIRECTIONS.SOUTH.value);
+      done();
+    });
+    it("should move right and the new direction is west",(done)=>{
+      moveRight(currCoordinates);
+      expect(currCoordinates).to.have.own.property('direction');
+      expect(currCoordinates.direction).to.equal(DIRECTIONS.WEST.value);
+      done();
+    });
+    it("should move right and the new direction is north",(done)=>{
+      moveRight(currCoordinates);
+      expect(currCoordinates).to.have.own.property('direction');
+      expect(currCoordinates.direction).to.equal(DIRECTIONS.NORTH.value);
+      done();
+    });
+    it("should move right and the new direction is east",(done)=>{
+      moveRight(currCoordinates);
+      expect(currCoordinates).to.have.own.property('direction');
+      expect(currCoordinates.direction).to.equal(DIRECTIONS.EAST.value);
+      done();
+    });
   })
+  
 })
